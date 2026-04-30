364e27f4a203b46d9ba0ed2ee5bf7b338710417e
duck <113956421+duckcantcode@users.noreply.github.com>
2026-04-29 20:46:25 -0400

    update noctalia

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf5 -y install https://repos.fyralabs.com/terra43/noctalia-qs-0%3A0.0.10-1.fc43.x86_64.rpm
+dnf5 -y install https://repos.fyralabs.com/terra43/noctalia-qs-0%3A0.0.12-2.fc43.x86_64.rpm
 
 # setup niri
 dnf5 -y install					\
@@ -20,7 +20,7 @@ dnf5 -y install					\
 	xdg-desktop-portal-gnome	\
 	gnome-keyring				\
 	fuzzel						\
-	https://repos.fyralabs.com/terra43/noctalia-shell-0%3A4.7.1-1.fc43.x86_64.rpm				\
+	https://repos.fyralabs.com/terra43/noctalia-shell-0%3A4.7.6-1.fc43.x86_64.rpm				\
 	swayidle					\
 	swaylock					\
 	polkit-kde					\
